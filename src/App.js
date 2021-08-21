ac5d6e61361104791fecab7970ba94d1f220a570
Tilda Lu <tilda.lu@trunk-studio.com>
2021-08-21 14:35:42 +0800

    update node, add blue h1 example

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import './MyFirstSass.scss';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>this is blue h1</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
