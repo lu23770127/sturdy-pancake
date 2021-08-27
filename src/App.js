fe77c266abcfde98f0e2fa28b835ca7e1e7a5cfe
Tilda Lu <tilda.lu@trunk-studio.com>
2021-08-27 16:23:52 +0800

    feat: :art: test nesting style
    h1 with link should have different color

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
-import './MyFirstSass.scss';
+import './MyFirstSass.sass';
+import './main.sass';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>this is blue h1</h1>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>this is blue h1
+          <a href="http://google.com.tw">test</a>
+        </h1>
+        <h2>this is variable h2</h2>
       </header>
     </div>
   );
