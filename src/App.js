7d5f36b4d1954ede97d7df13f352bc3734823703
Tilda Lu <tilda.lu@trunk-studio.com>
2021-09-02 10:25:59 +0800

    feat: :sparkles: mixin
    using mixin method to try darkmode theme

--- src/App.js
+++ src/App.js
@@ -1,33 +1,14 @@
-import logo from "./logo.svg";
 import "./App.css";
-import "./assets/sass/MyFirstSass.sass";
-import "./assets/sass/main.sass";
-import "./assets/sass/nestingError.sass";
+import "./assets/sass/mixin.sass";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>
-          this is blue h1
-          <a href="http://google.com.tw">test</a>
-        </h1>
-        <h2>this is variable h2</h2>
-
-        <ul>
-          <li>
-            first li <span>this is span, smaller than li</span>
-          </li>
-          <li>second li</li>
-          <li>
-            third with <a href="https://google.com.tw">link!</a>
-          </li>
-          <li>
-            forth with label <label>hi this is label</label>
-          </li>
-        </ul>
-        <h4>hi</h4>
+        <label class="info">this is info</label>
+        <label class="success">this is success</label>
+        <label class="warning">this is warning</label>
+        <label class="danger">this is danger</label>
       </header>
     </div>
   );
