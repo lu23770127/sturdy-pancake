a2dc3c0da1e309f058778ef937632ed8529706a0
Tilda Lu <tilda.lu@trunk-studio.com>
2021-09-02 17:16:17 +0800

    feat: :art: partial & module
    make a drawing with partial module method

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import "./App.css";
-import "./assets/sass/mixin.sass";
+import "./assets/sass/module.sass";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <label class="info">this is info</label>
-        <label class="success">this is success</label>
-        <label class="warning">this is warning</label>
-        <label class="danger">this is danger</label>
+        <div>
+          cloud cloud cloud <label>sun</label> cloud
+          <br /> cloud cloud cloud
+          <p>mud mud mud</p>
+        </div>
       </header>
     </div>
   );
