754e0630fec3cbf91d7d4d6ad52b535b405a98c5
Tilda Lu <tilda.lu@trunk-studio.com>
2021-09-13 21:03:52 +0800

    feat: :art: header
    header styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
-import "../src/assets/sass/basic.sass";
+import "../src/assets/sass/main.sass";
 function App() {
-  return <div className="container">VOGUE</div>;
+  return (
+    <div className="container">
+      <div className="header">
+        <label className="logo">VOGUE</label>
+        <label className="menu">
+          <label className="menu-item">
+            <select name="location" id="location">
+              <option value="Taiwan">Taiwan</option>
+              <option value="HongKong">HongKong</option>
+            </select>
+            membership hamberger
+          </label>
+        </label>
+      </div>
+    </div>
+  );
 }
 
 export default App;
